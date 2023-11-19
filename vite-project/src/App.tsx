73f9dff0f5535435eb754fda8d76f1a219723d7c
Fazilet Özisci <Fazilet.oezisci@hotmail.de>
2023-11-19 16:20:03 +0100

    extracted login component from app page

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,85 +1,9 @@
-import React, { useState } from "react";
-
+import React from "react";
+import { Login } from "./Login";
 import "./App.css";
-import {
-  Button,
-  Card,
-  Checkbox,
-  FormControl,
-  FormControlLabel,
-  FormGroup,
-  Grid,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  OutlinedInput,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 function App() {
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleMouseDownPassword = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    event.preventDefault();
-  };
-
-  return (
-    <>
-      <Card className="card">
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h2">Welcome Back!</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField fullWidth label="Name" variant="outlined"></TextField>
-          </Grid>
-          <Grid item xs={12}>
-            <FormControl sx={{ width: "100%" }} variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-password">
-                Password
-              </InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-password"
-                type={showPassword ? "text" : "password"}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                label="Password"
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={12}>
-            <FormGroup>
-              <FormControlLabel
-                control={<Checkbox defaultChecked />}
-                label="Remember Me"
-              />
-            </FormGroup>
-          </Grid>
-          <Grid item xs={12}>
-            <Button fullWidth variant="contained">
-              Mui Button
-            </Button>
-          </Grid>
-        </Grid>
-      </Card>
-    </>
-  );
+  return <Login></Login>;
 }
 
 export default App;
